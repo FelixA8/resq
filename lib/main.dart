d99f24b328df134f6a5feda0e3170242f98503b8
Felix Anderson <121009387+FelixA8@users.noreply.github.com>
2025-08-02 14:05:04 +0700

    firebase-configuration

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,7 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 78, 71, 70)),
       ),
       home: const MyHomePage(title: 'ResQ Home Page'),
     );
